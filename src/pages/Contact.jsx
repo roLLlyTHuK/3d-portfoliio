38f38a67633812bbc82d6c30523c98f5fb1c4e76
roLLlyTHuK <71319633+roLLlyTHuK@users.noreply.github.com>
2024-09-18 23:06:24 +0300

    add useAlert, custom alert

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,12 +3,15 @@ import emailjs from "@emailjs/browser"
 import { Canvas } from "@react-three/fiber";
 import Fox from "../models/Fox";
 import Loader from "../components/Loader";
+import useAlert from "../hooks/useAlert";
+import Alert from "../components/Alert";
 
 const Contact = () => {
     const formRef = useRef(null);
     const [form, setForm] = useState({ name: ``, email: ``, message: `` });
     const [isLoading, setIsLoading] = useState(false);
     const [currentAnimations, setCurrentAnimations] = useState('idle');
+    const { alert, showAlert, hideAlert } = useAlert();
 
     const handleChange = (e) => {
         setForm({ ...form, [e.target.name]: e.target.value });
@@ -31,16 +34,17 @@ const Contact = () => {
             import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY,
         ).then(() => {
             setIsLoading(false);
-            alert("Thank you. I will get back to you as soon as possible.");
+            showAlert("Thank you. I will get back to you as soon as possible.", "success");
             setTimeout(() => {
+                hideAlert();
                 setForm({ name: ``, email: ``, message: `` });
                 setCurrentAnimations('idle');
             }, 2000);
         }).catch((error) => {
             setIsLoading(false);
             setCurrentAnimations('idle');
-            alert("Something went wrong. Please try again later.");
-            console.log('FAILED...', error);
+            showAlert('I did not receive your message. Please try again', "danger");
+            console.log('Error:', error.text);
         });
     };
     const handleFocus = () => setCurrentAnimations('walk');
@@ -48,6 +52,10 @@ const Contact = () => {
 
     return (
         <section className="relative flex lg:flex-row flex-col max-container">
+
+            {alert.show && <Alert {...alert} />}
+
+
             <div className="flex-1 min-w-[50%] flex flex-col">
                 <h1 className="head-text">Get in Touch</h1>
                 <form
