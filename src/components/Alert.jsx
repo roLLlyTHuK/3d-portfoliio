34632a327e40cd0419008fc28c834018c8f5b0d8
roLLlyTHuK <71319633+roLLlyTHuK@users.noreply.github.com>
2024-09-19 09:28:01 +0300

    fix alert popup

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -2,7 +2,7 @@ const Alert = ({ type, text }) => {
     return (
         <div className="absolute top-10 left-0 right-0 flex justify-center items-center">
             <div className={`${alert.type === 'danger' ? 'bg-red-800' : 'bg-blue-800'} 
-                p-2 text-indigo-100 leading-none lg:rounded-full flex lg:inline-flex`}
+                p-2 text-indigo-100 leading-none lg:rounded-full flex justify-center items-center lg:inline-flex`}
                 role="alert">
                 <p className={`${alert.type === 'danger' ? 'bg-red-500' : 'bg-blue-500'}
                     flex rounded-full uppercase px-2 py-1 font-semibold mr-3 text-xs`}
